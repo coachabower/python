3eaa82cd7707bc67ce5424fceef5d8b23adb4f2c
Ryan <kjappdevelopment@gmail.com>
2018-08-12 21:45:56 -0500

    test 3

--- first.py
+++ first.py
@@ -1,1 +1,42 @@
-print "hello World!"
+import second
+import json
+
+def main():
+    second.second_print();
+
+    import types
+    if type(second.fib()) == types.GeneratorType:
+        counter = 0
+        for n in second.fib():
+            if counter == 10:
+                break
+            print n
+            counter = counter + 1
+
+    float_array = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+    print(second.comprehension(float_array))
+
+    if second.countArgs(1,2,3,4) == 1:
+        print "good"
+    if second.countArgs(1,2,3,4,5) == 2:
+        print "better"
+    if second.isMagic(1,2,3,magicnumber=6) == False:
+        print "Great"
+    if second.isMagic(1,2,3,magicnumber=7) == True:
+        print "Awesome!"
+
+    aSet = set(['Jake','John','Eric'])
+    bSet = set(['John','Jill'])
+    print aSet.difference(bSet)
+    print aSet.symmetric_difference(bSet)
+    print aSet.intersection(bSet)
+    print aSet.union(bSet)
+
+    salaries = '{"Alfred":300,"Jane":400}'
+    new_salaries = second.add_employee(salaries, "Me",800)
+    decoded_salaries = json.loads(new_salaries)
+    print(decoded_salaries["Alfred"])
+    print(decoded_salaries["Jane"])
+    print(decoded_salaries["Me"])
+
+main()
--- second.py
+++ second.py
@@ -1,1 +1,37 @@
-print "skillz"
+import json
+
+def second_print():
+    print("In Second")
+
+def fib():
+    a = 1
+    b = 1
+
+    yield 1
+    yield 1
+    while True:
+        a,b = b,a+b
+        yield b
+
+def comprehension(list):
+    new_list = []
+    new_list = [li for li in list if li >= 0]
+    return new_list
+
+def countArgs (a,b,c,*d):
+    return len(list(d))
+
+def isMagic (a,b,c,**d):
+    if d.get('magicnumber') == 7:
+        return True
+    else:
+        return False
+
+def add_employee(salaries_json,name,salary):
+    salaries = json.loads(salaries_json)
+    try:
+        salaries[name] = salary
+    except IndexError:
+        salaries.append(name)
+        salaries[name] = salary
+    return json.dumps(salaries)
